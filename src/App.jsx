f5d82f59e6c0c31625c51d1e1f199c4e9d328fc2
imbekrishna <136263294+imbekrishna@users.noreply.github.com>
2024-02-04 17:11:23 +0530

    added react import

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import Categories from './pages/Categories';
 import Entertainment from './pages/Entertainment';
--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import NewsCard from '../../components/NewsCard';
 import NoteCard from '../../components/NoteCard';
